51c78241ad8e33f4fa5b23739556cd2deab7fa6e
chetan <chetansinghnegi512@gmail.com>
2022-12-27 15:19:42 +0530

    This is my static ecommerce website

---
+++ js/script.js
@@ -0,0 +1,22 @@
+  function searchproducts(){
+    let filter = document.getElementById('find').value.toUpperCase();
+
+    let item = document.querySelectorAll('.card');
+
+    let l = document.getElementsByTagName('h6');
+
+    for(var i = 
+        0;i<=l.length;i++){
+      let a=item[i].getElementsByTagName('h6')[0];
+
+      let value=a.innerHTML || a.innerText || a.textContent;
+
+      if(value.toUpperCase().indexOf(filter) > -1){
+        item[i].style.display='';
+      }
+      else{
+        item[i].style.display='none';
+      }
+    }
+
+  }
